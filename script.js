3b202aa60d3d2ec3ba8209a0d31003226dfebb4d
Aswin Asok <aswinasokofficial@gmail.com>
2020-12-10 07:40:02 +0530

    Rotataing Nav

--- script.js
+++ script.js
@@ -72,3 +72,12 @@ function update() {
     next.disabled = false;
   }
 }
+
+
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const content = document.querySelector('.content')
+
+open.addEventListener('click', () => content.classList.add('show-nav'))
+
+close.addEventListener('click', () => content.classList.remove('show-nav'))
