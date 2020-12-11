54616f414db9a30c0d8897f90d7e295385621e9e
Aswin Asok <aswinasokofficial@gmail.com>
2020-12-11 15:24:29 +0530

    Blurry Effect Integrated

--- script.js
+++ script.js
@@ -85,13 +85,38 @@ const input = document.querySelector(".input");
 
 sbtn.addEventListener("click", () => {
   if (input.value == "") {
-    search.classList.toggle('active');
-  }else{
-    panels[input.value-1].click();
+    search.classList.toggle("active");
+  } else {
+    panels[input.value - 1].click();
     input.value = "";
-    search.classList.toggle('active');
+    search.classList.toggle("active");
   }
-  
-  
+
   input.focus();
 });
+
+
+/*Blurry*/
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector(".main-container");
+
+let load = 0;
+
+let int =  setInterval(blurring ,30)
+function blurring() {
+    load++
+
+
+    if(load > 99) {
+        clearInterval(int)
+    }
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100 ,1 , 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30 ,0)}px)`
+}
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
